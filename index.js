ba8f6a0bab926ac5c9bf196fe811e041a82b8031
Cristian Florea <cristianfloreadev@gmail.com>
2026-02-17 17:32:06 +0100

    changed server

--- index.js
+++ index.js
@@ -257,8 +257,9 @@ connectDB()
     // Start campaign scheduler
     campaignScheduler.start();
 
-    server.listen(3000, () => {
-      console.log("Server running on port 3000");
+    const PORT = process.env.PORT || 3000;
+    server.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
     });
   })
   .catch((err) => {
